8a9696b1449888c7544f06c02a2635ee2307ef13
Kevin Castro <kevin.castro.chavez0@gmail.com>
2024-05-20 09:48:31 -0600

    setup swagger and created endpoints for get and post using ef

--- Program.cs
+++ Program.cs
@@ -1,6 +1,38 @@
+using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using PizzaStoreEF.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "PizzaStoreEF API",
+        Description = "Making the Pizzas you love",
+        Version = "v1"
+    });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
+    });
+}
+
+app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
+app.MapPost("/pizza", async (PizzaDb db, Pizza pizza) =>
+{
+    await db.Pizzas.AddAsync(pizza);
+    await db.SaveChangesAsync();
+    return Results.Created($"/pizza/{pizza.Id}", pizza);
+});
 
 app.Run();
